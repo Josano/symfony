4604955a36f6a2ecad227aaaa584fbd3a71cd354
Josano Soares <josano@hotmail.com>
2015-01-29 23:51:29 -0200

    Alterações para fase 3

--- src/Code/CarBundle/Controller/CarController.php
+++ src/Code/CarBundle/Controller/CarController.php
@@ -5,6 +5,9 @@ namespace Code\CarBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Code\CarBundle\Entity\Fabricante;
+use Code\CarBundle\Entity\Carro;
 
 class CarController extends Controller
 {
@@ -15,8 +18,7 @@ class CarController extends Controller
      */
     public function indexAction()
     {
-        $nome = 'Caio Weber Vidon de Souza';
-        return $this->render('CodeCarBundle:Car:index.html.twig',['nome'=>$nome]);
+        return $this->render('CodeCarBundle:Car:index.html.twig');
     }
 
     /**
@@ -32,5 +34,73 @@ class CarController extends Controller
     					  "PORSCHE"=>"911 Turbo Cabriolet 3.6/3.8","FERRARI"=>"F599 GTB Fiorano F1 6.0 V12 620cv"); 
 
         return array('veiculos' => $veiculos);
-    }    
+    }  
+
+    /**
+     * @Route("/adicionarFabricante")
+     * @Template()
+     */
+    public function adicionarFabricanteAction(){
+
+        $fabricantes = array("FIAT", "BMW", "CITROEN", "ASTON MARTIN", "MASERATI","GM - CHEVROLET", "VW - VOLKSVAGEN",
+                             "TOYOTA", "PORSCHE","FERRARI"); 
+
+        $em = $this->getDoctrine()->getEntityManager();
+
+        foreach($fabricantes as $fabricante){
+          $novoFabricante = new Fabricante();
+          $novoFabricante->setNome($fabricante);
+          $em->persist($novoFabricante);  
+          $em->flush(); 
+        }
+
+        return new Response('Fabricantes cadastrados com sucesso.');
+        
+    }
+
+    /**
+     * @Route("/adicionarModelo")
+     * @Template()
+     */
+    public function adicionarModeloAction(){
+
+        $carros = array(
+                          array("modelo" => "147 C/ CL", "fabricante" => 1, "ano" => 1980, "cor"=> "BRANCO"),
+                          array("modelo" => "120iA 2.0 16V 156cv 3p", "fabricante" => 2, "ano" => 2009, "cor"=> "AZUL"),
+                          array("modelo" => "AIRCROSS TENDANCE 1.6 Flex 16V 5p Mec.", "fabricante" => 3, "ano" => 2014, "cor"=> "AMARELO"),
+                          array("modelo" => "Rapide S 6.0 V12 550cv", "fabricante" => 4, "ano" => 2011, "cor"=> "VERDE"),
+                          array("modelo" => "GranTurismo S 4.7 V8 32V/ MC Line", "fabricante" => 5, "ano" => 2012, "cor"=> "PRETO"),
+                          array("modelo" => "AGILE LTZ EASYTRONIC 1.4 8V", "fabricante" => 6, "ano" => 2008, "cor"=> "CINZA"),
+                          array("modelo" => "AMAROK CD2.0 16V/S CD2.0 16V TDI 4x2 Die", "fabricante" => 7, "ano" => 2014, "cor"=> "PRATA"),
+                          array("modelo" => "Corolla XEi 1.8/1.8", "fabricante" => 8, "ano" => 2015, "cor"=> "ROXO"),
+                          array("modelo" => "911 Turbo Cabriolet 3.6/3.8", "fabricante" => 9, "ano" => 2003, "cor"=> "PRETO"),
+                          array("modelo" => "F599 GTB Fiorano F1 6.0 V12 620cv", "fabricante" => 10, "ano" => 2015, "cor"=> "VERMELHO"),                                
+                       );
+
+        $em = $this->getDoctrine()->getEntityManager();
+
+        for($i=0;$i<count($carros);$i++){
+            $novoModelo = new Carro();
+            $c=0;
+            foreach($carros[$i] as $key=>$value){
+                $c++;
+                if($key == 'modelo'){
+                    $novoModelo->setModelo($value);
+                }else if($key == 'fabricante'){
+                          $novoModelo->setFabricante($value);      
+                }else if($key == 'ano') {
+                          $novoModelo->setAno($value);
+                }else{
+                        $novoModelo->setCor($value);
+                }   
+            }
+            
+            $em->persist($novoModelo);  
+            $em->flush();  
+        }
+
+        return new Response('Modelos cadastrados com sucesso.');
+        
+    }
+
 }
---
+++ src/Code/CarBundle/Entity/Fabricante.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Code\CarBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *@ORM\Table(name="fabricante")
+ *@ORM\Entity 
+ */
+class Fabricante
+{
+	/**
+     * @ORM\Column(type="integer", length=11)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+	private $id;
+
+	 /**
+     * @ORM\Column(name="nome", type="string", length=255)
+     */
+	private $nome;
+
+
+
+    /**
+     * Gets the value of id.
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets the value of id.
+     *
+     * @param mixed $id the id
+     *
+     * @return self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of nome.
+     *
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Sets the value of nome.
+     *
+     * @param mixed $nome the nome
+     *
+     * @return self
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+}
